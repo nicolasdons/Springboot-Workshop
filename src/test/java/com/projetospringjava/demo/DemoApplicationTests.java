afe5fe7e840bab360be85e205f3b0c3011cc2951
Nicolas <nicolasnssoares@gmail.com>
2026-04-29 10:36:39 -0300

    projeto criado

---
+++ src/test/java/com/projetospringjava/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetospringjava.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
